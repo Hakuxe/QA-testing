ff456b2a2d6bb11026e22948a5d2b1349efa79b9
Hakuxe <arthurnorton.jobs@gmail.com>
2022-12-12 22:05:21 -0300

    chore: testing command chaining

--- basic-cypress/cypress/e2e/basic.cy.ts
+++ basic-cypress/cypress/e2e/basic.cy.ts
@@ -10,13 +10,11 @@ describe("Basic testing ", ()=>{
 
       cy.get(".search-keyword").type("ca");
 
-      cy.get(".products").find(".product:visible");
-
-
-
       cy.get(".product").should("be.visible").and("have.length.at.least", 2);
-
       //another option
       cy.get(".product:visible").should("have.length.at.least", 1);
+      
+
+      cy.get(".products").find(".product").eq(2).contains("add to cart", {matchCase:false}).click();
    })
 });
