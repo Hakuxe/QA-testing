9b40f0a55a3be5a88b79e02da16f2d9ef3e1a668
Hakuxe <arthurnorton.jobs@gmail.com>
2022-12-13 22:21:24 -0300

    change: finding product with text carrot

--- basic-cypress/cypress/e2e/basic.cy.ts
+++ basic-cypress/cypress/e2e/basic.cy.ts
@@ -1,20 +1,52 @@
-describe("Basic testing ", ()=>{
+describe("Basic testing ", () => {
+	// some cool links
+	// https://rahulshettyacademy.com/AutomationPractice/
+	// https://rahulshettyacademy.com/seleniumPractise/#/
 
-   // some cool links
-   // https://rahulshettyacademy.com/AutomationPractice/
-   // https://rahulshettyacademy.com/seleniumPractise/#/
+	it.only("search input should return at least one element", () => {
+		cy.visit("https://rahulshettyacademy.com/seleniumPractise/#/");
+
+		cy.get(".search-keyword").type("ca");
+
+		cy.get(".product").should("be.visible").and("have.length.at.least", 2);
+
+		//another option
+		cy.get(".product:visible").should("have.length.at.least", 1);
+
+      cy.get(".products").as("productsLocator");
 
 
-   it.only("search input should return at least one element",() =>{
-      cy.visit("https://rahulshettyacademy.com/seleniumPractise/#/");
+      // pick second product and add1s to cart
+		cy.get("@productsLocator")
+			.find(".product")
+			.eq(2)
+			.contains("add to cart", { matchCase: false })
+			.click();
 
-      cy.get(".search-keyword").type("ca");
+		//find carrots
+		cy.get("@productsLocator")
+			.find(".product")
+			.each(($element, index, $list) => {
+				const productName = $element
+					.find("h4.product-name")
+					.text()
+					.toLocaleLowerCase();
 
-      cy.get(".product").should("be.visible").and("have.length.at.least", 2);
-      //another option
-      cy.get(".product:visible").should("have.length.at.least", 1);
+				if (productName.includes("carrot")) {
+					cy.wrap($element).find("button").click();
+				}
+			});
+
+         //cypress uses promises on every command
+
       
+      //check brand text
 
-      cy.get(".products").find(".product").eq(2).contains("add to cart", {matchCase:false}).click();
-   })
+      cy.get(".brand").should("have.text", "GREENKART");
+
+      cy.get(".brand").then($element =>{
+         console.log($element.text());
+      })
+         
+	});
 });
