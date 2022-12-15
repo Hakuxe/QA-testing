2ecb9d369057eaf3bdd690426a72d82771912102
Hakuxe <arthurnorton.jobs@gmail.com>
2022-12-14 22:08:49 -0300

    refactor: changing configuration and file name

--- basic-cypress/cypress.config.ts
+++ basic-cypress/cypress.config.ts
@@ -5,6 +5,8 @@ export default defineConfig({
 		setupNodeEvents(on, config) {
 			// implement node event listeners here
 		},
+		viewportWidth: 1280,
+		viewportHeight: 720,
 		specPattern: [
 			"cypress/e2e/**/*.cy.{js,jsx,ts,tsx}",
 			"cypress/e2e/**/*.spec.{js,jsx,ts,tsx}", //pode colocar o padrão que desejar
