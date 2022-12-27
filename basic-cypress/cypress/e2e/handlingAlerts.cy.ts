8747308871967f050964ef04e53e73ddd2a22a4c
Hakuxe <arthurnorton.jobs@gmail.com>
2022-12-26 22:02:48 -0300

    handling alerts with cypress

---
+++ basic-cypress/cypress/e2e/handlingAlerts.cy.ts
@@ -0,0 +1,20 @@
+describe("Learning how to handle alert messages", () => {
+	it("handling alter pop-up", () => {
+      const url = "https://rahulshettyacademy.com/AutomationPractice/";
+
+      cy.visit(url);
+
+      //cypress handle popups by default
+      cy.get("#alertbtn").click();
+      cy.get("input[value='Confirm']").click();
+
+      // listening to events on the browser
+      cy.on("window:alert", (stringOfTheAlert) =>{
+         expect(stringOfTheAlert).include("Hello , share this practice page and share your knowledge");
+      });
+
+      cy.on("window:confirm", (textOfTheConfirmPopup) =>{
+         expect(textOfTheConfirmPopup).include("Hello , Are you sure you want to confirm?");
+      });
+   });
+});
