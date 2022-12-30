a417210e9188c1ac7c445dc1fb0668a92d5110ce
Hakuxe <arthurnorton.jobs@gmail.com>
2022-12-29 22:02:34 -0300

    testing web tables

---
+++ basic-cypress/cypress/e2e/webTables.cy.ts
@@ -0,0 +1,38 @@
+describe("handling web tables", () => {
+	const url = "https://rahulshettyacademy.com/AutomationPractice/";
+
+	it("should check if selenium price is 25", () => {
+		cy.visit(url);
+
+		cy.get("#product")
+			.find("td")
+			.each((item, index, list) => {
+				if (
+					item.text() ==
+					"Master Selenium Automation in simple Python Language"
+				) {
+					// cy.next pick the next sibling element
+					cy.wrap(item)
+						.next()
+						.then((price) => {
+							expect(price.text()).to.eq("25");
+						});
+					//expect(cy.get('td').eq(index).next()).to.eq("25");
+				}
+			});
+
+      // another solution
+		cy.get("tr td:nth-child(2)").each(($e1, index, $list) => {
+			const text = $e1.text();
+			if (text.includes("Python")) {
+				cy.get("tr td:nth-child(2)")
+					.eq(index)
+					.next()
+					.then(function (price) {
+						const priceText = price.text();
+						expect(priceText).to.equal("25");
+					});
+			}
+		});
+	});
+});
