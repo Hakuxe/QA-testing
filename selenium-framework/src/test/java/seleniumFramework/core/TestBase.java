3cd90943dd449e483c7f6954a66efaf5ce630268
Hakuxe <arthurnorton.jobs@gmail.com>
2023-08-30 21:27:07 -0300

    adding screenshots to tests

--- selenium-framework/src/test/java/seleniumFramework/core/TestBase.java
+++ selenium-framework/src/test/java/seleniumFramework/core/TestBase.java
@@ -1,19 +1,41 @@
 package seleniumFramework.core;
 
+import com.sun.org.apache.xpath.internal.axes.FilterExprWalker;
+import org.apache.commons.io.FileUtils;
+import org.openqa.selenium.OutputType;
+import org.openqa.selenium.TakesScreenshot;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
+
+import java.io.File;
+import java.io.IOException;
+import java.lang.reflect.Method;
 
 public class TestBase {
     // base para criação dos tests
 
+    private String testName = "";
+
     @BeforeMethod
-    public void beforeTest(){
+    public void beforeTest(Method method){
         DriverFactory.createDriver();
+        testName = method.getName();
         DriverFactory.DRIVER.manage().window().maximize();
+
+        String url = "/src/test/resourses/componentes.html";
+
+        DriverFactory.DRIVER.get("file:///" + System.getProperty("user.dir") + url);
     }
 
     @AfterMethod
     public void afterTest() {
-       DriverFactory.quitDriver();
+        TakesScreenshot screenshot = (TakesScreenshot) DriverFactory.DRIVER;
+        File file = screenshot.getScreenshotAs(OutputType.FILE);
+        try {
+            FileUtils.copyFile(file, new File("target" + File.separator + "screenshots" + File.separator + testName +".jpg"));
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+        DriverFactory.quitDriver();
     }
 }
--- selenium-framework/src/test/java/seleniumFramework/pages/MainPage.java
+++ selenium-framework/src/test/java/seleniumFramework/pages/MainPage.java
@@ -1,6 +1,26 @@
 package seleniumFramework.pages;
 
+import org.openqa.selenium.By;
 import seleniumFramework.core.PageBase;
 
 public class MainPage extends PageBase {
+
+    By button = By.id("buttonDelay");
+    By input = By.id("novoCampo");
+
+    public void clickButton(){
+        click(button);
+    }
+
+    public void fillInputSlowResponse(String text){
+        sendKeys(input, text);
+    }
+
+    public String getValueInputSlowResponse(){
+        return getValueInput(input);
+    }
+
+    public void waitForInput(long time){
+        waitForElementByTime(input,time);
+    }
 }
