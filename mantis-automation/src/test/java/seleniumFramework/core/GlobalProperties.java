1999c93ea69ba4ae705add6c659777b0cdb3a816
Hakuxe <arthurnorton.jobs@gmail.com>
2023-11-23 00:10:07 -0300

    feat: finishing test createAPublicIssue

--- mantis-automation/src/test/java/seleniumFramework/core/GlobalProperties.java
+++ mantis-automation/src/test/java/seleniumFramework/core/GlobalProperties.java
@@ -12,23 +12,23 @@ public class GlobalProperties {
 
     public static String BROWSER_DEFAULT = "FIREFOX";
     public static String URL = "https://mantis-prova.base2.com.br/bug_report_page.php";
-    public static String DEFAULT_USER;
-    public static String DEFAULT_PASSWORD;
+    public static String DEFAULT_USER = "Arthur Norton";
+    public static String DEFAULT_PASSWORD = "base2Test";
     public static long TIMEOUT = 30;
 
-    public GlobalProperties() {
-        Properties properties = new Properties();
-
-        try {
-            InputStream inputStream = Files.newInputStream(Paths.get("local.properties"));
-            properties.load(inputStream);
-        } catch (IOException error){
-            error.printStackTrace();
-        }
-
-        DEFAULT_USER = properties.getProperty("user");
-        DEFAULT_PASSWORD = properties.getProperty("password");
-    }
+//    public GlobalProperties() {
+//        Properties properties = new Properties();
+//
+//        try {
+//            InputStream inputStream = Files.newInputStream(Paths.get("local.properties"));
+//            properties.load(inputStream);
+//        } catch (IOException error){
+//            error.printStackTrace();
+//        }
+//
+//        DEFAULT_USER = properties.getProperty("user");
+//        DEFAULT_PASSWORD = properties.getProperty("password");
+//    }
 
 
 }
--- mantis-automation/src/test/java/seleniumFramework/tests/IssueTests.java
+++ mantis-automation/src/test/java/seleniumFramework/tests/IssueTests.java
@@ -8,6 +8,7 @@ import seleniumFramework.core.TestBase;
 import seleniumFramework.flows.LoginFlows;
 import seleniumFramework.pages.IssuePage;
 import seleniumFramework.pages.LoginPage;
+import seleniumFramework.utils.Utils;
 
 public class IssueTests extends TestBase {
     //    Testes
@@ -36,42 +37,38 @@ public class IssueTests extends TestBase {
         String summary = "Esta é uma issue aberta por meio de teste com selenium 22-11";
         String description = "Esta é a descrição da issue aberta por meio de teste com selenium 22-11";
         String stepReproduce = "Passo a passo para reproduzir os erros......";
-        String additionalInfo = "Info adicional vai aki ";
+        String additionalInfo = "Info adicional vai aki";
 
         String expectedSuccessMessage = "Operation successful.";
 
         issuePage.goToPageReportIssue();
         issuePage.clickSelectProjectBtn();
 
-        issuePage.selectReproducibilityByText("always");
-        issuePage.selectSeverityByText("major");
-        issuePage.selectPriorityByText("high");
-        issuePage.fillSummary("Esta é uma issue aberta por meio de teste com selenium 22-11");
-        issuePage.fillDescription("Esta é a descrição da issue aberta por meio de teste com selenium 22-11");
-        issuePage.fillStepsToReproduce("Passo a passo para reproduzir os erros......");
-        issuePage.fillAdditionalInfo("Info adicional vai aki ");
+        issuePage.selectReproducibilityByText(reproducibility);
+        issuePage.selectSeverityByText(severity);
+        issuePage.selectPriorityByText(priority);
+        issuePage.fillSummary(summary);
+        issuePage.fillDescription(description);
+        issuePage.fillStepsToReproduce(stepReproduce);
+        issuePage.fillAdditionalInfo(additionalInfo);
         issuePage.clickSubmitIssueBtn();
 
         Assert.assertEquals(issuePage.getTextCreatedIssueSuccessAlert(), expectedSuccessMessage);
 
 
         Assert.assertEquals(issuePage.getTextIssueViewStatus(), viewStatus);
-//        Assert.assertEquals(issuePage.getTextIssueSubmittedDate(),);
+
+        String actualDate = issuePage.getTextIssueSubmittedDate().split(" ")[0];
+        Assert.assertEquals(actualDate, Utils.getNowDate("yyyy-MM-dd"));
+
         Assert.assertEquals(issuePage.getTextIssuePriority(),priority);
         Assert.assertEquals(issuePage.getTextIssueSeverity(),severity);
         Assert.assertEquals(issuePage.getTextIssueReproducibility(),reproducibility);
-
-        // TODO o número da issue fica na frente do summary
-        Assert.assertEquals(issuePage.getTextIssueSummary(),summary);
+        Assert.assertTrue(issuePage.getTextIssueSummary().contains(summary));
         Assert.assertEquals(issuePage.getTextIssueDescription(),description);
         Assert.assertEquals(issuePage.getTextIssueStepsToReproduce(),stepReproduce);
         Assert.assertEquals(issuePage.getTextIssueAdditionalInfo(),additionalInfo);
 
-        // confirir se os dados estão batendo na issue
-
-
-
-        System.out.println("aki");
     }
 
 }
