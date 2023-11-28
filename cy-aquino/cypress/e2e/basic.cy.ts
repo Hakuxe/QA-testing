6b0b382c46eac0704dc5e3c8e587d4571b9fe734
Hakuxe <arthurnorton.jobs@gmail.com>
2023-11-27 22:16:57 -0300

    feat: adding hooks

--- cy-aquino/cypress/e2e/basic.cy.ts
+++ cy-aquino/cypress/e2e/basic.cy.ts
@@ -1,14 +1,19 @@
 describe("Basic comands", () => {
-	it("go to page and check title ", () => {
-		cy.visit("https://wcaquino.me/cypress/componentes.html");
 
+	before(() => {
+		cy.log("Antes de todos os tests")
+	})
+	
+	beforeEach(() =>{
+		cy.visit("https://wcaquino.me/cypress/componentes.html");
+	})
+
+	it("go to page and check title ", () => {
 		cy.title().should("equal", "Campo de Treinamento");
 		cy.title().should("contain", "Campo");
 	});
 
-	it.only("click on button and take value", () => {
-		cy.visit("https://wcaquino.me/cypress/componentes.html");
-
+	it("click on button and take value", () => {
 		cy.get("input[value='Clique Me!']");
 		cy.get("#buttonSimple").click().should("contain.value", "Obrigado!");
 	});
