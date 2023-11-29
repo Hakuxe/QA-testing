5d54435e1eb8be4bf0cd2ca1effceb9916891112
Hakuxe <arthurnorton.jobs@gmail.com>
2023-11-28 22:04:02 -0300

    feat: studying text fields

--- cy-aquino/cypress/e2e/basic.cy.ts
+++ cy-aquino/cypress/e2e/basic.cy.ts
@@ -1,12 +1,11 @@
 describe("Basic comands", () => {
-
 	before(() => {
-		cy.log("Antes de todos os tests")
-	})
-	
-	beforeEach(() =>{
+		cy.log("Antes de todos os tests");
+	});
+
+	beforeEach(() => {
 		cy.visit("https://wcaquino.me/cypress/componentes.html");
-	})
+	});
 
 	it("go to page and check title ", () => {
 		cy.title().should("equal", "Campo de Treinamento");
@@ -17,4 +16,24 @@ describe("Basic comands", () => {
 		cy.get("input[value='Clique Me!']");
 		cy.get("#buttonSimple").click().should("contain.value", "Obrigado!");
 	});
+
+	it.only("should fill text input", () => {
+		const name = "Sara";
+
+		cy.get("#formNome").type(name).should("contain.value", name);
+
+		cy.get("#elementosForm\\:sugestoes").type(name);
+		cy.get("#elementosForm\\:sugestoes").should("have.value", name);
+
+		cy.get("[data-cy='dataSobrenome']")
+			.type("12345{backspace}{backspace}")
+			.should("have.value", "123");
+
+		cy.get("#elementosForm\\:sugestoes")
+			.clear()
+			.type("novo nome", { delay: 100 })
+			.should("contain.value", "novo nome");
+	});
+
+	
 });
