98d2666218f5aad0971ec746d436a27ac6241133
Hakuxe <arthurnorton.jobs@gmail.com>
2024-01-13 12:34:44 -0300

    feat: adding assert in tests

--- cy-aquino/cypress/e2e/fixture.cy.ts
+++ cy-aquino/cypress/e2e/fixture.cy.ts
@@ -1,15 +1,14 @@
 // error : Cannot find module '@fixtures/user.json'. Did you mean to set the 'moduleResolution' option to 'NodeNext', or to add aliases to the 'paths' option?
 
 // change the cypress/tsconfig json  to moduleResolution": "NodeNext",
-import user from "@fixtures/user.json";
-
+import user from "../fixtures/user.json";
 
 describe("test using fixture", () => {
 	beforeEach(() => {
 		cy.visit("https://wcaquino.me/cypress/componentes.html");
 	});
 
-	it.only("should fill the form using fixtures", () => {
+	it("should fill the form using fixtures", () => {
 		cy.fixture<userFixture>("user").then((user) => {
 			cy.get("#formNome").type(user.name);
 			cy.get("#formSobrenome").type(user.surname);
@@ -17,16 +16,21 @@ describe("test using fixture", () => {
 			cy.get("[name='formComidaFavorita']").check(user.favoriteFood);
 			cy.get("[data-test='dataEscolaridade']").select(user.educationLevel);
 			cy.get("#formEsportes").select(user.sports);
+
+			cy.get("#formCadastrar").click();
+			cy.get("#resultado span").first().should("have.text", "Cadastrado!");
 		});
 	});
 
-	it.skip("using import statement", () => {
-
+	it("using import statement", () => {
 		cy.get("#formNome").type(user.name);
 		cy.get("#formSobrenome").type(user.surname);
 		cy.get("[name='formSexo']").check(user.gender);
 		cy.get("[name='formComidaFavorita']").check(user.favoriteFood);
 		cy.get("[data-test='dataEscolaridade']").select(user.educationLevel);
 		cy.get("#formEsportes").select(user.sports);
+
+		cy.get("#formCadastrar").click();
+		cy.get("#resultado span").first().should("have.text", "Cadastrado!");
 	});
 });
