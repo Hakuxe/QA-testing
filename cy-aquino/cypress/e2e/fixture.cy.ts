d8c15b6d1985220475340be875f38baa6a6dd991
Hakuxe <arthurnorton.jobs@gmail.com>
2024-05-17 06:50:32 -0300

    feature: adding fixture to cypress tests

--- cy-aquino/cypress/e2e/fixture.cy.ts
+++ cy-aquino/cypress/e2e/fixture.cy.ts
@@ -4,48 +4,48 @@
 import user from "../fixtures/user.json";
 
 describe("test using fixture", () => {
-	beforeEach(() => {
-		cy.visit("https://wcaquino.me/cypress/componentes.html");
-	});
+  beforeEach(() => {
+    cy.visit("https://wcaquino.me/cypress/componentes.html");
+  });
 
-	it("should fill the form using fixtures", () => {
-		cy.fixture<userFixture>("user").then((user) => {
-			cy.get("#formNome").type(user.name);
-			cy.get("#formSobrenome").type(user.surname);
-			cy.get("[name='formSexo']").check(user.gender);
-			cy.get("[name='formComidaFavorita']").check(user.favoriteFood);
-			cy.get("[data-test='dataEscolaridade']").select(user.educationLevel);
-			cy.get("#formEsportes").select(user.sports);
+  it("should fill the form using fixtures", () => {
+    cy.fixture<userFixture>("user").then((user) => {
+      cy.get("#formNome").type(user.name);
+      cy.get("#formSobrenome").type(user.surname);
+      cy.get("[name='formSexo']").check(user.gender);
+      cy.get("[name='formComidaFavorita']").check(user.favoriteFood);
+      cy.get("[data-test='dataEscolaridade']").select(user.educationLevel);
+      cy.get("#formEsportes").select(user.sports);
 
-			cy.get("#formCadastrar").click();
-			cy.get("#resultado span").first().should("have.text", "Cadastrado!");
-		});
-	});
+      cy.get("#formCadastrar").click();
+      cy.get("#resultado span").first().should("have.text", "Cadastrado!");
+    });
+  });
 
-	it("using import statement", () => {
-		cy.get("#formNome").type(user.name);
-		cy.get("#formSobrenome").type(user.surname);
-		cy.get("[name='formSexo']").check(user.gender);
-		cy.get("[name='formComidaFavorita']").check(user.favoriteFood);
-		cy.get("[data-test='dataEscolaridade']").select(user.educationLevel);
-		cy.get("#formEsportes").select(user.sports);
+  it("using import statement", () => {
+    cy.get("#formNome").type(user.name);
+    cy.get("#formSobrenome").type(user.surname);
+    cy.get("[name='formSexo']").check(user.gender);
+    cy.get("[name='formComidaFavorita']").check(user.favoriteFood);
+    cy.get("[data-test='dataEscolaridade']").select(user.educationLevel);
+    cy.get("#formEsportes").select(user.sports);
 
-		cy.get("#formCadastrar").click();
-		cy.get("#resultado span").first().should("have.text", "Cadastrado!");
-	});
+    cy.get("#formCadastrar").click();
+    cy.get("#resultado span").first().should("have.text", "Cadastrado!");
+  });
 
-	it.only("should use a fixture using ts file ", () => {
-		cy.fixture("user.js").then((data) => {
-			console.log(data);
-			cy.get("#formNome").type(user.name);
-			cy.get("#formSobrenome").type(user.surname);
-			cy.get("[name='formSexo']").check(user.gender);
-			cy.get("[name='formComidaFavorita']").check(user.favoriteFood);
-			cy.get("[data-test='dataEscolaridade']").select(user.educationLevel);
-			cy.get("#formEsportes").select(user.sports);
+  it.only("should use a fixture using ts file ", () => {
+    cy.fixture("user.js").then((data) => {
+      console.log(data);
+      cy.get("#formNome").type(user.name);
+      cy.get("#formSobrenome").type(user.surname);
+      cy.get("[name='formSexo']").check(user.gender);
+      cy.get("[name='formComidaFavorita']").check(user.favoriteFood);
+      cy.get("[data-test='dataEscolaridade']").select(user.educationLevel);
+      cy.get("#formEsportes").select(user.sports);
 
-			cy.get("#formCadastrar").click();
-			cy.get("#resultado span").first().should("have.text", "Cadastrado!");
-		});
-	});
+      cy.get("#formCadastrar").click();
+      cy.get("#resultado span").first().should("have.text", "Cadastrado!");
+    });
+  });
 });
