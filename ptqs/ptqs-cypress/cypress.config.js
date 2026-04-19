a311547c28be7a5eae4851887cffc87997b0f40c
Hakuxe <arthurnorton.jobs@gmail.com>
2026-04-19 13:21:35 -0300

    fix: removing node_modules

--- ptqs/ptqs-cypress/cypress.config.js
+++ ptqs/ptqs-cypress/cypress.config.js
@@ -2,10 +2,11 @@ const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
   allowCypressEnv: false,
-
+  reporter: "cypress-mochawesome-reporter",
   e2e: {
-    video:true,
+    video: true,
     setupNodeEvents(on, config) {
+      require('cypress-mochawesome-reporter/plugin')(on);
       // implement node event listeners here
     },
   },
--- ptqs/ptqs-cypress/cypress/support/commands/common.js
+++ ptqs/ptqs-cypress/cypress/support/commands/common.js
@@ -1,9 +1,9 @@
-Cypress.Commands.add("validateToastMessage", (expectedMessage) => { 
+Cypress.Commands.add("validateToastMessage", (expectedMessage) => {
   cy.get(".toast").should("be.visible").should("have.text", expectedMessage);
 });
 
-Cypress.Commands.add('selecionarOpcaoNaCombobox', (labelDoCampo, opcao) => {
-    cy.get(`label[for="${labelDoCampo}"]`).parent().as(`campo-${labelDoCampo}`)
-    cy.get(`@campo-${labelDoCampo}`).click()
-    cy.get(`@campo-${labelDoCampo}`).contains(opcao).click()
-})
+Cypress.Commands.add("selecionarOpcaoNaCombobox", (labelDoCampo, opcao) => {
+  cy.get(`label[for="${labelDoCampo}"]`).parent().as(`campo-${labelDoCampo}`);
+  cy.get(`@campo-${labelDoCampo}`).click();
+  cy.get(`@campo-${labelDoCampo}`).contains(opcao).click();
+});
