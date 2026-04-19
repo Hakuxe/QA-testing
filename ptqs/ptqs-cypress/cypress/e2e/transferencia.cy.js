e5bc12ce46b817cde9c4d0b0014ccba9835ce123
Hakuxe <arthurnorton.jobs@gmail.com>
2026-04-19 13:42:23 -0300

    fix: changing data on tranferencia file

--- ptqs/ptqs-cypress/cypress/e2e/transferencia.cy.js
+++ ptqs/ptqs-cypress/cypress/e2e/transferencia.cy.js
@@ -2,18 +2,17 @@ describe("template spec", () => {
   beforeEach(() => {
     cy.visit("http://localhost:4000/");
 
-     cy.login("julio.lima", "123456A");
+    cy.login("julio.lima", "123456");
   });
 
-  it.only("Deve transferir quando informo dados e valor validos", () => {
+  it("Deve transferir quando informo dados e valor validos", () => {
     cy.realizarTransferencia("João da Silva", "Maria Oliveira", "11");
-  
+
     cy.validateToastMessage("Transferência realizada!");
   });
 
   it("Deve apresentar erro quando tentar transferir mais que 5 mil sem o token", () => {
     cy.realizarTransferencia("João da Silva", "Maria Oliveira", "5001");
-     cy.validateToastMessage("Transferência realizada!")
+    cy.validateToastMessage("Autenticação necessária para transferências acima de R$5.000,00.");
   });
 });
-
